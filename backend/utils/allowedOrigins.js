2628a096e53129bba86bd19d2d0c2e18791c6b96
Bon Lacorte <bonbonlacorte123@gmail.com>
2024-02-14 00:50:42 +0800

    allowed-origins

--- backend/utils/allowedOrigins.js
+++ backend/utils/allowedOrigins.js
@@ -3,7 +3,11 @@ const allowedOrigins = [
     'http://localhost:3501',
     'http://localhost:3502',
     'http://127.0.0.1:5173',
-    'http://localhost:5173'
+    'http://localhost:5173',
+    'https://pupfind2.onrender.com',
+    'https://pupfind-bons-projects-fcaae0da.vercel.app',
+    'https://pupfind.vercel.app'
+
 ]
 
 export default allowedOrigins
